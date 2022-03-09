9e4198b400837a6b525bf6f67d163e10b683172b
Lukasz Wolnik <lukasz@wolnik.co.uk>
2022-03-10 00:14:47 +0100

    Initial Commit

---
+++ Pay360Test/Pay360TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pay360TestApp.swift
+//  Pay360Test
+//
+//  Created by Lukasz on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pay360TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
